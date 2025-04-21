9b10f3a7be45c6f5aa93685f400ff7b2377f2c14
tiedsandi <fachransandi@gmail.com>
2025-04-21 09:39:07 +0700

    create login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserRoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::get('logout', [AuthController::class, 'logout']);
+Route::post('action-login', [AuthController::class, 'actionLogin']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('user', UserRoleController::class);
 });
